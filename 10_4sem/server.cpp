2d75f1c1044803e155adecdf4944bd203bf9e81d
Vadim <nikitin.vl@phystech.edu>
2021-04-15 18:47:59 +0300

    Добавлена функция чтения

--- 10_4sem/server.cpp
+++ 10_4sem/server.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+void read_information_while(boost::asio::ip::tcp::socket& socket);
+
 int main()
 {
+
+}
+
+void read_information_while(boost::asio::ip::tcp::socket& socket)
+{
+    boost::asio::streambuf buffer;
+    std::string message;
+
+    do
+    {
+        boost::asio::read_until(socket, buffer, '\n');
+
+        std::istream input_stream(&buffer);
+        std::getline(input_stream, message, '\n');
+
+        std::cout << message << std::endl;
+    } while (message != "The conversation is ended");
 
 }
